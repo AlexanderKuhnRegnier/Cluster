4f66a882d3da41c2811e146c5621f9679948de57
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2016-06-28 17:33:06 +0100

    Added command line input capability to stage1 code, removed all uses of [], as all input data is now taken from command line input, not the URL

--- Stripped + Commented Processing Files/stage1_processing_commented_stripped.php
+++ Stripped + Commented Processing Files/stage1_processing_commented_stripped.php
@@ -56,7 +56,6 @@ function fgetb($handle)
 }
 /*
 $base="C".$sc."_".$shortyear.$month.$day."_".$version;
-$sc - the spacecraft number, retrieved from URL via $_GET["sc"];
 */
 #outputdata: string appended to by output function, contains xyz components in engineering units, measurement range and comment
 #extblockcount: number of extended mode data blocks
@@ -522,6 +521,85 @@ function packet_analyse($bytes,&$state,&$startreset,&$stopreset)
 
 // Program start
 
+//Acquiring the selection info from the command line input now, not from the URL!
+
+$day = null;
+$month = null;
+$year = null;
+$sc = null;
+$version = null;
+
+$shortopts  = "d:";
+$shortopts .= "m:";
+$shortopts .= "y:";
+$shortopts .= "sc:";
+$shortopts .= "ver:";
+
+$options = getopt($shortopts);
+#var_dump($options);
+
+if (array_key_exists("y",$options)){$year  = $options["y"];}
+else {exit("Please select a Year".PHP_EOL);}
+if (array_key_exists("m",$options)){$month = $options["m"];}
+else {exit("Please select a month".PHP_EOL);}
+if (array_key_exists("d",$options)){$day   = $options["d"];}
+else {exit("Please select a Day".PHP_EOL);}
+
+#verification of input parameters, php automatically converts between string and int
+if (array_key_exists("sc",$options))
+{
+	if ($sc > 4 || $sc < 1)
+	{
+		exit("Invalid Spacecraft".PHP_EOL);
+	}
+	else {$sc = $options["sc"];}
+}
+else
+{
+	echo "Setting default value for Spacecraft: Rumba (1)".PHP_EOL;
+	$sc = 1;
+}
+if (array_key_exists("ver",$options))
+{
+	if (strtoupper($version) != 'A' || strtoupper($version) != 'B' || strtoupper($version) != 'K')
+	{
+		exit("Invalid Version".PHP_EOL);
+	}
+	else {$version = $option["ver"];}
+}
+else
+{
+	echo "Setting default value for Version: B".PHP_EOL;
+	$version = 'B';
+}
+if ($month > 12 || $month < 1)
+{
+	exit("Invalid Month".PHP_EOL);
+}
+#possible issue with date()
+if ($year < 2000)
+{
+	$year += 2000;
+}
+if ($year > date("Y") || $year < 2000)
+{
+	exit("Invalid Year".PHP_EOL);
+}
+#beware of lacking support for calendars, but seems to be working (24/06/2016)
+if ($options["d"] > cal_days_in_month(CAL_GREGORIAN,$month,$year) || $options["d"] < 1)
+{
+	exit("Number of days is invalid".PHP_EOL);
+}
+
+$jdcount = gregoriantojd($month, $day, $year);
+$month_name = jdmonthname($jdcount,0);
+
+echo "Year:       ".$year.PHP_EOL;
+echo "Month:      ".$month_name.PHP_EOL;
+echo "Day:        ".$day.PHP_EOL;
+echo "Spacecraft: ".$sc.PHP_EOL;
+echo "Version:    ".$version.PHP_EOL;
+
 $outputdata=array();
 
 set_time_limit(600);									#Sets maximum execution time limit
@@ -530,17 +608,9 @@ set_time_limit(600);									#Sets maximum execution time limit
 define("EXT",'/home/ahk114/extended/'); 				#writing the meta files to my own home directory on the server
 define("RAW",'/cluster/data/raw/');
 
-require("headfoot.php");								#headfoot.php and meta_file_functions.php found in root directory www
-
-if ($_GET["year"]<2000)									#gets year from selection - from URL entry - only have to consider years 2000+
-	$year=$_GET["year"]+2000;							
-else
-	$year=$_GET["year"];
 $shortyear=sprintf("%02d",$year-2000);			
-$month=sprintf("%02d",$_GET["month"]);
-$day=sprintf("%02d",$_GET["day"]);
-$sc=$_GET["sc"];
-$version=$_GET["version"];
+$month=sprintf("%02d",$month);
+$day=sprintf("%02d",$day);
 
 $base="C".$sc."_".$shortyear.$month.$day."_".$version;
 $datafilename=RAW.$year."/".$month."/".$base.".BS";		#burst science (BS) raw data file
