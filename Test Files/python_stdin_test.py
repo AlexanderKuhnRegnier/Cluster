54afa0aaa3a55e8a1185f80ca3b01d06a95c4e48
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2016-06-28 15:25:41 +0100

    Writing to temp file successful

--- Test Files/python_stdin_test.py
+++ Test Files/python_stdin_test.py
@@ -1,8 +1,20 @@
 import fileinput
+import tempfile
+
+tmpfile = tempfile.TemporaryFile(mode='wb+')
+print tmpfile.closed
 counter = 0 
 for line in fileinput.input():
-    counter += 1
-    print "Line: ", counter
-    print line,
-    
+    print "Writing: ", line,
+    tmpfile.write(line)
+print ""
+print "Finished Writing"
+print "Contents of temporary file"
+tmpfile.seek(0) #have to seek in order to see any output!!!
+print tmpfile.read()
+
+print "Closing tmpfile"
+tmpfile.close()    
+print tmpfile.closed
+
 print "Finished"
