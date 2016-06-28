a713bc12b4fb61a63869b2e26eb7b6d700f2bc1f
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2016-06-28 17:22:03 +0100

    Updated the command line input module to produce all necessary information. This will now be inserted into the stage1 processing code

--- Test Files/command_line_input_test.php
+++ Test Files/command_line_input_test.php
@@ -8,18 +8,57 @@ echo PHP_EOL;
 
 #var_dump($argv);
 
+//Acquiring the selection info from the command line input now, not from the URL!
+
+$day = null;
+$month = null;
+$year = null;
+$sc = null;
+$version = null;
+
 $shortopts  = "d:";
 $shortopts .= "m:";
 $shortopts .= "y:";
+$shortopts .= "sc:";
+$shortopts .= "ver:";
 
 $options = getopt($shortopts);
-var_dump($options);
-$month = $options["m"];
-$year  = $options["y"];
-$day   = $options["d"];
-#verification of input parameters, php automatically converts between
-#string and int
+#var_dump($options);
 
+if (array_key_exists("y",$options)){$year  = $options["y"];}
+else {exit("Please select a Year".PHP_EOL);}
+if (array_key_exists("m",$options)){$month = $options["m"];}
+else {exit("Please select a month".PHP_EOL);}
+if (array_key_exists("d",$options)){$day   = $options["d"];}
+else {exit("Please select a Day".PHP_EOL);}
+
+#verification of input parameters, php automatically converts between string and int
+if (array_key_exists("sc",$options))
+{
+	if ($sc > 4 || $sc < 1)
+	{
+		exit("Invalid Spacecraft".PHP_EOL);
+	}
+	else {$sc = $options["sc"];}
+}
+else
+{
+	echo "Setting default value for Spacecraft: Rumba (1)".PHP_EOL;
+	$sc = 1;
+}
+if (array_key_exists("ver",$options))
+{
+	if (strtoupper($version) != 'A' || strtoupper($version) != 'B' || strtoupper($version) != 'K')
+	{
+		exit("Invalid Version".PHP_EOL);
+	}
+	else {$version = $option["ver"];}
+}
+else
+{
+	echo "Setting default value for Version: B".PHP_EOL;
+	$version = 'B';
+}
 if ($month > 12 || $month < 1)
 {
 	exit("Invalid Month".PHP_EOL);
@@ -29,7 +68,6 @@ if ($year > date("Y") || $year < 2000)
 {
 	exit("Invalid Year".PHP_EOL);
 }
-
 #beware of lacking support for calendars, but seems to be working (24/06/2016)
 if ($options["d"] > cal_days_in_month(CAL_GREGORIAN,$month,$year) || $options["d"] < 1)
 {
@@ -39,10 +77,11 @@ if ($options["d"] > cal_days_in_month(CAL_GREGORIAN,$month,$year) || $options["d
 $jdcount = gregoriantojd($month, $day, $year);
 $month_name = jdmonthname($jdcount,0);
 
-echo "Year: ".$year.PHP_EOL;
-echo "Month: ".$month_name.PHP_EOL;
-echo "Day: ".$day.PHP_EOL;
-
+echo "Year:       ".$year.PHP_EOL;
+echo "Month:      ".$month_name.PHP_EOL;
+echo "Day:        ".$day.PHP_EOL;
+echo "Spacecraft: ".$sc.PHP_EOL;
+echo "Version:    ".$version.PHP_EOL;
 
 echo PHP_EOL;
 session_destroy();
