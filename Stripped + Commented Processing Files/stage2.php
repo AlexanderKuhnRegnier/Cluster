9e3f1963244226750d2ad6189082438747bd7e57
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2016-06-29 13:16:01 +0100

    Successful extraction of filepath from meta file from stage1 from stdin, as opposed to URL. Output from stage1 contains the desired path, prefixed with the string 'target' which is taken as the start of the 41 character long filepath in the stage2 code.

--- Stripped + Commented Processing Files/stage2.php
+++ Stripped + Commented Processing Files/stage2.php
@@ -4,11 +4,15 @@
 // comments are the original in line comments added by Tim Oddy when he originally wrote the code, circa 2001.
 ##AKR commments
 
+/*
+Delete the following 3 lines, AND LAST LINE for deployment, or figure out why permission to normal session folder are denied
+*/
+session_destroy();
+ini_set('session.save_path','/home/ahk114'. '/testing/'. 'session/'); 
+session_start();
 $verbose=FALSE;																	#global boolean, set if debugging required, ignore commands involving it
 
-require 'headfoot.php';															#php file constructing the HTML banners
-
-head("cluster");
+echo "Starting Stage2".PHP_EOL;
 
 require 'meta_file_functions.php';
 
@@ -18,8 +22,13 @@ define("INITIATE",1);
 define("TERMINATE",2);
 define("RAW","/cluster/data/raw/");												#shortcut to raw data directory
 
-$filename=$_GET["filename"];													#This assigns the variable to the information in the web address. Automatically super global.
-																				#$filename is only then changed in one scope when using the corresponding .SCCH file
+#eg. /cluster/data/extended/2016/01/C1_160101_B
+# in my case, /home/ahk114/extended/2016/01/C1_160101_B
+$stdin_input=file_get_contents("php://stdin",'r');
+$filename = substr($stdin_input,strpos($stdin_input, 'target')+6,41);			#now extracts filename from stdin, not URL
+
+exit("Testing");
+
 function fgetb($handle)
 	/** Returns the ASCII value of the current character in the handle (Handle is often the .SCCH file). ##.SCCH files are S/C command files.
 	
@@ -93,8 +102,6 @@ function lastextendedmode($unixtime,$sc,$type)
 				rsort($line);													##sorts array from highest to lowest (reverse order)
 				for($m=0;$m<count($line);$m++)									#iterating over the entire length of the array, but breaks at first entry
 				{
-					if (strstr($line[$m],"FGM") && $verbose)
-						echo "<FONT SIZE=-3>".$line[$m]."</FONT><BR>";
 					if (strstr($searchstring,substr($line[$m],61,8)))			#checks column 61 of the copy of the line in the .SCCH file. This is where the extended mode switch command will be
 					{
 						$event=mktime(substr($line[$m],11,2),substr($line[$m],14,2),substr($line[$m],17,2),substr($line[$m],5,2),substr($line[$m],8,2),substr($line[$m],0,4));
@@ -157,65 +164,35 @@ function spin($unixtime,$sc)
 		return 4;
 }
 
-
-// echo $filename.".META";
-
 $numberofblocks=read_meta($filename.".META","NumberOfBlocks");				#stage1_processing.php calculates the block count in extended mode and writes it to the .META file
 
 $bits=explode("/",$filename);												#Each section (defined by the separator /) is written as separate elements of an array
 
 $sc=substr($bits[count($bits)-1],1,1);										#searches the sections of the HTML web address for the spacecraft number.
 
-echo '<H1>Meta File: '.$filename.'</H1>';									#header across the web page of file
-
 for($n=0;$n<$numberofblocks;$n++)											#iterate over the number of blocks in the extended mode data as found by stage1_processing.php
 {
-	echo "<B>Block ".$n."</B><BR>";
 	$start=lastextendedmode(read_meta($filename.".META","DumpStartTime_Unix",$n),$sc,INITIATE);
-	echo "$start";
 	$end=lastextendedmode(read_meta($filename.".META","DumpStartTime_Unix",$n),$sc,TERMINATE);
 	$part=read_meta($filename.".META","PartialStart",$n);
-	echo "Dump Start ".read_meta($filename.".META","DumpStartTime_ISO",$n)."<BR>";		#Gets the extended mode command times using the function above
-	if ($part=="TRUE")
-		echo "<FONT COLOR=\"#F000C0\">Probable repeat copy</FONT><BR>";					#prints a pink alert on the webpage of partial block ??What is part? Partial block? Partial extended mode duration overlap in time?
-	echo "Extended Mode Entry: ".date("Y-m-d\TH:i:s\Z",$start)."<BR>";
 	$cary["Extended Mode Entry "."$n".":"] = date("Y-m-d\TH:i:s\Z",$start);
-	echo "Extended Mode Exit: ".date("Y-m-d\TH:i:s\Z",$end)."<BR>";
 	$cary["Extended Mode Exit "."$n".":"] = date("Y-m-d\TH:i:s\Z",$end);
-	echo "Extended Mode Duration: ".date("H:i:s",$end-$start)."<BR>";					#duration time found from subtracting start from the end time
 	$cary["Extended Mode Duration "."$n".":"] = date("H:i:s",$end-$start);
 	$spin=spin($start,$sc);
-	printf("Spin period: %0.6f<BR>",$spin);												#this is acquired from the .SATT file
 	$cary["spin period "."$n".":"] = $spin;
 	$calcvec=(int)(($end-$start)/$spin);												#number of vectors found from duration of extended mode and spin period NOT from extended mode data.
-	$actualvec=read_meta($filename.".META","NumberOfVectors",$n);						#number of vectors as found from the extended mode data via stage1_processing.php
-	echo "Derived Number of vectors : ".$calcvec."<BR>";
-	echo "Number of vectors in block : ".$actualvec."<BR>";
-	
+	$actualvec=read_meta($filename.".META","NumberOfVectors",$n);						#number of vectors as found from the extended mode data via stage1_processing.php	
 	$carykeys = array_keys($cary);
 	$caryvalues = array_values($cary);
 	$m = count($cary);
 	for($i=0; $i<=($m-1); $i++)
 		$caryfinal[] = "$carykeys[$i]"." "."$caryvalues[$i]";
 	
-	if (abs($calcvec-$actualvec)>0)
-	{
-		if (abs($calcvec-$actualvec)>10)												#If expected number of vectors (from .SATT) is greater than the actual data recieved by 10 then;
-			echo "<FONT COLOR=\"RED\">";												#?? Why 10 the threshold for a cause for alarm, arbitrary choice?
-		else
-			echo "<FONT COLOR=\"BLACK\">";
-		echo "Timing suggests ".(int)(($calcvec-$actualvec)/444.5)." packets missing";	#???444.5 number of vectors in a packet in extended mode. As BM3 packet contains 3562 bytes of data one complete vector is 64 bits long.
-		if ($part)
-			echo " - This block is partial, so this is not unexpected";					#??Cause for difference is always blamed on the partial block. Is this always the case?
-		echo "</FONT><BR>";																#linebreak in HTML regardless of error messages posted or not
-	}
 	$miss=read_meta($filename.".META","MissingPacket",$n);								#variable created in stage1_processing.php
-	if ($miss!=0)
-		echo "<FONT COLOR=RED>State machine suggests, at least ".$miss." packets missing</FONT><BR>";	#?? state machine involved in stage1_processing.php
+
 	$reset_start=read_meta($filename.".META","ResetCountStart",$n);						#Counts of the 5.152s reset pulse sent to instrument. Found in stage1_processing.php
 	$reset_stop=read_meta($filename.".META","ResetCountEnd",$n);
-	printf("Reset range: %d-%d / %03X-%03X<BR>",$reset_start,$reset_stop,$reset_start,$reset_stop);		#prints reset range in decimal and hexadecimal 
-	echo "<P>";																					#finishes by printing small white space on webpage
+
 	write_meta($filename.".META","ExtendedModeEntry_ISO",date("Y-m-d\TH:i:s\Z",$start),$n);		#?? used later on- but not utilised at the moment
 	write_meta($filename.".META","ExtendedModeEntry_Unix",$start,$n);
 	write_meta($filename.".META","ExtendedModeExit_ISO",date("Y-m-d\TH:i:s\Z",$end),$n);
@@ -227,6 +204,6 @@ for($n=0;$n<$numberofblocks;$n++)											#iterate over the number of blocks i
 	file_put_contents("$caryfilename"."_information.txt", $impcary);					#saves to my www directory - permission denied in my own directory
 }
 
-foot("cluster");
 
+session_destroy();
 ?>
