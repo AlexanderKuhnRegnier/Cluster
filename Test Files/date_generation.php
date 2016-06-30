a7c6a6ae1f1daf3877a017e67b2de439f0ac29a9
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2016-06-30 12:23:31 +0100

    Small mod to date generation test file

--- Test Files/date_generation.php
+++ Test Files/date_generation.php
@@ -12,9 +12,10 @@ $year = 2016;
 for ($i=0;$i<1;$i+=1)
 {
 	echo "Input date: ".date("Y/m/d",mktime(0,0,0,$month,$day,$year)).PHP_EOL;
-	$cmd = "php /home/ahk114/Cluster/Test\ Files/command_line_input_test.php -y".$year." -m".$month." -d".$day;	
-	echo "Executing: ".$cmd.PHP_EOL;
-	exec($cmd,$output);
+	$option_string = " -y".$year." -m".$month." -d".$day;
+	$cmd = "php /home/ahk114/Cluster/Test\ Files/command_line_input_test.php";	
+	echo "Executing: ".$cmd.$option_string.PHP_EOL;
+	exec($cmd.$option_string,$output);
 	var_dump($output);
 	echo "Output: ".PHP_EOL.$output;
 }
