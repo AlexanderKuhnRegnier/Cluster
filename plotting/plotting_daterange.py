6ef96c9bc186adb77d78a028cc7f6939379627e4
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2016-07-07 14:31:53 +0100

    Implemented finding, decompressing and plotting of caa data, in addition to plotting of reference data

--- plotting/plotting_daterange.py
+++ plotting/plotting_daterange.py
@@ -3,17 +3,20 @@ from bokeh.plotting import figure, output_file, show
 from numpy import linalg as LA
 from datetime import date,time,datetime,timedelta
 import os
-from getcaafile import getcaafile
+from getfile import getfile
+import gzip
 
 class vector:
     def __init__(self):
         self.v = 0
         self.datetime = 0
         self.magnitude = 0
-    def assigndatetime(self,datetime):
-        self.datetime=datetime        
+    def assigndatetime(self,dt):
+        self.datetime=dt        
     def assignvalue(self,v):
         self.v = v
+    def assignmagnitude(self,mag):
+        self.magnitude = mag
     def calcmagnitude(self):
         self.magnitude = LA.norm(self.v)
     
@@ -37,7 +40,21 @@ class vectorlist:
     def read_file(self,filename,limit=-1):
         if '.cef.gz' in filename and '/caa/ic_archive/' in filename: #this is caa file, needs to be read differently!
             self.filename=filename
-            
+            #gzip.open(filename) #defaults to 'rb' and 9
+            with gzip.open(self.filename, 'rb') as f:
+                for line in f:
+                    if 'DATA_UNTIL = EOF' in line:
+                        co=0
+                        for line in f:
+                            co+=1
+                            if co==limit:
+                                break
+                            line = line.split(',')
+                            v = vector()                            
+                            v.assigndatetime(np.datetime64(line[0]))
+                            v.assignvalue(np.array(line[2:5],dtype=np.float64))
+                            v.assignmagnitude(float(line[5]))
+                            self.add_vector_entry(v)
         else:  
             self.filename = filename
             counter = 0
@@ -61,6 +78,12 @@ class vectorlist:
         return [vector.datetime for vector in self.vectors]
     def returnmagnitudes(self):
         return [vector.magnitude for vector in self.vectors]
+    def returnx(self):
+        return [vector.v[0] for vector in self.vectors]
+    def returny(self):
+        return [vector.v[1] for vector in self.vectors]
+    def returnz(self):
+        return [vector.v[2] for vector in self.vectors]
     def plotlist(self,color='blue',legend='magnitude'):#outdated
         dates = self.returndatetimes()
         magnitudes = self.returnmagnitudes()
@@ -72,54 +95,68 @@ class vectorlist:
         p.line(dates, magnitudes, color=color, legend=legend)
         # show the results
         show(p)   
-    def plotlists(self,array,scatter=False,log=True):
+    def plotlists(self,array,scatter=True,log=False):
+        labels = [','.join([entry[2],entry[3]]) for entry in array]
+        labels = '-'.join(set(labels))
+        #array = [vlist,color,legend,plotwhich]
         # output to static HTML file (with CDN resources)
-        output_file("plot.html", title="Plotting Cluster II Data", mode="cdn")
-        
+        output_file(labels+".html", title="Plotting Cluster II Data", mode="cdn")
         TOOLS="resize,crosshair,pan,wheel_zoom,box_zoom,reset"
-        
         # create a new plot with the tools above, and explicit ranges
         if log:
             p = figure(tools=TOOLS, width=1900, height=1000, y_axis_type='log', x_axis_type="datetime")
         else:
             p = figure(tools=TOOLS, width=1900, height=1000, x_axis_type="datetime")
         for entry in array:
+            #print "entry ",entry
             vlist = entry[0]
             color = entry[1]
             legend = entry[2]
+            if len(entry)==4:
+                plotwhich= entry[3]
+            else:
+                plotwhich = ''
             #checks could be done here
             #for now, just assume everything will be ok
             dates = vlist.returndatetimes()
-            magnitudes = vlist.returnmagnitudes()
-            if scatter:
-                p.circle(dates,magnitudes,color=color,legend=legend,size=1)
+            if plotwhich=='mag':
+                data = vlist.returnmagnitudes()
+            elif plotwhich=='x':
+                data = vlist.returnx()
+            elif plotwhich=='y':
+                data=vlist.returny()
+            elif plotwhich=='z':
+                data=vlist.returnz()
             else:
-                p.line(dates,magnitudes,color=color,legend=legend)
+                data=vlist.returnmagnitudes()
+            '''
+            print "data excerpt"
+            for i in range(5):
+                print dates[i],data[i],self.filename
+            '''
+            if scatter:
+                p.circle(dates,data,color=color,legend=legend+' ('+plotwhich+')',size=1)
+            else:
+                p.line(dates,data,color=color,legend=legend)
         p.xaxis.axis_label = 'Time'
         p.yaxis.axis_label = 'nT'
         show(p)
     def print_values(self,limit):
         counter = 0
+        print "filename:",self.filename
         for vector in self.vectors:
-            print vector.datetime, vector.magnitude
-            counter+=1
             if counter == limit:
-                break
+                break            
+            counter+=1
+            print vector.datetime, vector.v, vector.magnitude
 class vectorfiles:
     def __init__(self):
         self.array = []
-        
-    def add_vectorlist(self,vlist):
+    def add_vectorlist(self,vlist,color,legend,plotwhich='mag'):
         if not isinstance(vlist, vectorlist):
             raise Exception('Object supplied must be a vector list')
-        else:
-            if (vlist.ext):
-                colour = 'red'
-                legend = 'ext mode'
-            else:
-                colour = 'green'
-                legend = 'normal'           
-            self.array.append([vlist,colour,legend])
+        else:         
+            self.array.append([vlist,color,legend,plotwhich])
     def plotfiles(self,scatter=False,log=True):
         s = vectorlist()
         s.plotlists(self.array,scatter,log)
@@ -127,69 +164,68 @@ class vectorfiles:
         for array in self.array:
             #print array,array[0].filename
             print array[0].filename
+
+def plot(vfiles,sc,start_date,end_date,dirs,colours,legends,plotwhichs):
+    if start_date==end_date or end_date=='':
+        dates = start_date
+    else:
+        dates = [start_date+timedelta(days=1)*i for i in range(abs(end_date-start_date).days)]
+    for datev in dates:
+        print ""
+        print datev.isoformat()
+        Year = str(datev.year)
+        #year = Year[2:4]
+        month = '{0:02d}'.format(datev.month)
+        day = '{0:02d}'.format(datev.day)
+
+        for dir,colour,legend,plotwhich in zip(dirs,colours,legends,plotwhichs):
+            directory = dir+Year+'/'+month+'/'
+            if 'Y:' in directory:
+                file = getfile(sc,Year,month,day,directory,ext=True)
+            else:
+                file = getfile(sc,Year,month,day,directory)
+
+            if file:
+                print "filefound:",file
+                vlist = vectorlist()
+                vlist.read_file(file)
+                vfiles.add_vectorlist(vlist,colour,legend,plotwhich)
+                #vlist.print_values(0)
+            
+    #vfiles.printfiles()
+    if 'x' in plotwhichs or 'y' in plotwhichs or 'z' in plotwhichs:
+        log=False
+    else:
+        log=True
+    vfiles.plotfiles(scatter=True,log=log)
             
 #filename = "Y:/reference/2015/12/C1_151231_B.EXT.GSE"
-'''
-filename = "default.GSE"
 
-vlist1 = vectorlist()
-vlist1.read_file(filename)
-#vlist1.plotlist(color='red',legend='default')
 
-filename = "cal.GSE"
-
-vlist2 = vectorlist()
-vlist2.read_file(filename)
-#vlist2.plotlist(color='green',legend='caa')
-
-vlist2.plotlists([[vlist2,'green','caa'],[vlist1,'red','default']])
-'''
 vfiles = vectorfiles()
-reffolderahk114 = "Y:/reference/"
-reffolder = "Z:/data/reference/"
+refdirahk114 = "Y:/reference/"
+refdir = "Z:/data/reference/"
+caadir = 'Z:/caa/ic_archive/'
 sc = 1
-start_date = date(2016,01,9)
-end_date = date(2016,01,14)
-dates = [start_date+timedelta(days=1)*i for i in range(abs(end_date-start_date).days)]
-for datev in dates:
-    file_dict = {}
-    Year = str(datev.year)
-    year = Year[2:4]
-    month = '{0:02d}'.format(datev.month)
-    day = '{0:02d}'.format(datev.day)
-    
-    folderahk114 = reffolderahk114+Year+'/'+month+'/'
-    folder = reffolder+Year+'/'+month+'/'
-    
-    for file in os.listdir(folder):
-        if "C"+str(sc) in file and year+month+day in file and ".P.GSE" in file:
-            version = file[10]  
-            #print "version:",version
-            file_dict[version]=file
-    l = file_dict.keys()
-    if len(l):
-        #print "number of files:",len(l)
-        l.sort()
-        #print "final version:",l[-1]
-        #print ""
-        file = folder+file_dict[l[-1]]
-        vlist = vectorlist()
-        vlist.read_file(file)
-        vfiles.add_vectorlist(vlist)
-    
-    file_dict = {}
-    for file in os.listdir(folderahk114):
-        if "C"+str(sc) in file and year+month+day in file and ".EXT.GSE" in file:
-            version = file[10]            
-            file_dict[version]=file
-            
-    l = file_dict.keys()
-    if len(l):
-        l.sort()
-        file = folderahk114+file_dict[l[-1]]
-        vlist = vectorlist(ext=True)
-        vlist.read_file(file)
-        vfiles.add_vectorlist(vlist)   
 
-vfiles.printfiles()
-vfiles.plotfiles(scatter=True)
+end_date = ''
+start_date = date(2016,01,8)
+end_date = date(2016,01,14)
+
+dirs=[
+caadir,
+refdirahk114
+]
+colours=[
+'green',
+'red'
+]
+legends=[
+'caa','ext mode caa'
+]
+plotwhichs=[
+'mag',
+'mag'
+]
+
+plot(vfiles,sc,start_date,end_date,dirs,colours,legends,plotwhichs)
