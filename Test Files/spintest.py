c9549fe39c2b651bc6da3fd398e936b197cddd35
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2016-07-11 17:08:13 +0100

    Concluded modification of code, now uses numpy arrays and is thus much faster. Depending on the spacecraft, for the same reset pulse signal width, different frequencies are expected to be observed, as expected.

--- Test Files/spintest.py
+++ Test Files/spintest.py
@@ -1,118 +1,84 @@
 import numpy as np
+#import time
 #from math import pi
 import matplotlib.pyplot as plt
 
 
-def func(spin,widthreset=0.08):
-    widthspin=spin
+def func():
+    global n,spin,reset,resetwidth,dt,axarr
+    '''
+    print n
+    print spin
+    print reset
+    print resetwidth
+    print dt
+    print spin/dt
+    print reset/dt
+    print resetwidth/dt
+    '''
+    '''
+    if (spin/dt != int(spin/dt)) or (reset/dt != int(reset/dt)) or (resetwidth/dt != int(resetwidth/dt)) :
+        raise Exception("Choose appropriate spin,reset,resetwidth and dt values!")
+    '''
 
-    maxi = 415
-    #off = np.random.random()*spin
-    #print "offset:", off
-    x = np.linspace(0,maxi,num=maxi*100)+1.1
+    spindt = int(spin/dt)
+    resetdt = int(reset/dt)
+    resetwidthdt=int(resetwidth/dt)    
+    size = n*spindt
+    print "size:", size
+
     
     #start = time.time()
     #print "Start time:",start
     
-    def squarereset(x):
-        y = []
-        lim = int((max(x)/reset+1))
-        #lim = 200
-        for s in x:
-            for n in range(lim):
-                if (reset*n)-widthreset/2. < s < (reset*n)+widthreset/2.:
-                    y.append(1)
-                    break
-                else:
-                    if n==lim-1:
-                        y.append(0)
-        return y
-        
-    def spinreset(x):
-        y = []
-        lim = 200
-        for s in x:
-            for n in range(lim):
-                if (spin*n)-widthspin/2 < s < (spin*n)+widthspin/2:
-                    y.append(1)
-                    break
-                else:
-                    if n==lim-1:
-                        y.append(0)
-        return y    
-    #print squarereset(x)
-    def mean(x,y,interval):
-        
-        xs = []
-        ys = []
-        xmeans = []
-        ymeans = []
-        max_n = int(max(x)/interval) +1
-        #print "maxn:", max_n
-        for n in range(max_n):
-            for i,j in zip(x,y):
-                #print i,j
-                if interval*n < i < interval*(n+1):
-                    #print "2"
-                    #print i,j
-                    xs.append(i)
-                    ys.append(j)
     
-            xmeans.append(np.mean(xs))
-            ymeans.append(np.mean(ys))
-            xs = []
-            ys = []   
-            
-        #n = int(interval/((max(x)-min(x))/len(x)))
-        n=415
-        print "n:",n,len(x)
-        newx = np.asarray(x)
-        print len(newx),type(newx)
-        newx = newx.reshape(-1,n)
-        newxmean = np.mean(newx,axis=1)
-        newy = np.asarray(y)
-        newy = newy.reshape(-1,n)
-        newymean = np.mean(newy,axis=1)
-        for a,b,c,d in zip(xmeans,newxmean,ymeans,newymean):
-            print a,b,c,d
-        return xmeans,ymeans
+    reset_array = np.array([],dtype=np.float64).reshape(-1,1)
+    while(reset_array.shape[0]<size):
+        reset_array = np.append(reset_array,np.zeros((resetdt-resetwidthdt,1)))
+        reset_array = np.append(reset_array,np.ones((resetwidthdt,1)))
+    
+    if reset_array.shape[0]>size:
+        reset_array = reset_array[:size]
+    else:
+        raise Exception("Something went wrong with reset array")
         
+    y = reset_array
+    print "length y:", len(y),y.shape
+    y = np.asarray(y)
+    y = np.mean(y.reshape(-1,spindt),axis=1)
+    x = np.linspace(spin/2.,spin*n+spin/2.,num=n)
+    if len(x) != len(y):
+        raise Exception("Lengths don't match up",len(x),len(y))
+    
     '''
-    axarr[0].scatter(x,squarereset(x))
-    #axarr[1].scatter(x,spinreset(x))
+    for i,j in zip(x,y):
+        print i,j
     '''
     
-    x,y=mean(x,squarereset(x),spin)
-    '''
-    #print x
-    axarr[1].scatter(x,y,c='r',s=300)
-    
-    for ax in axarr:
-        ax.set_ylim((-0.2,1.2))
-        ax.set_xlim((-0.2,maxi+0.2))
-    axarr[1].set_ylim((-0.05*max(y),1.1*max(y)))
-    '''
+    #plt.scatter(x,y,s=100)    
+
     
     x_low = []
     for i,j in zip(x,y):
         if j < np.mean(y):
             x_low.append(i)
     print "                           spin:",spin
-    print "                     widthreset:",widthreset
+    print "                     widthreset:",resetwidth
     print "product of spin and reset times:", reset*spin
     print "  mean separation of low points:", (max(x_low)-min(x_low))/len(x_low)
     print "                     difference:", reset*spin-(max(x_low)-min(x_low))/len(x_low)
     
-    for axes in axarr:
-        axes[0].scatter(x,y)
-    '''
-    end = time.time()
-    print "Duration time:",end-start
-    '''
+    return x,y
+    
+    
+    #end = time.time()
+    #print "Duration time:",end-start
+    
+    
 
+#plt.close('all')
+f,axarr = plt.subplots(4,2)
 
-plt.close('all')
-f,axarr = plt.subplots(4,2,figsize=(20,15))
 '''
 #eg.
 axarr[0, 0].plot(x, y)
@@ -126,20 +92,40 @@ axarr[1, 1].set_title('Axis [1,1]')
 '''
 #import time
 reset = 5.152
-reset = 5.15
+
 #spin = 4.260732 #Rumba
 #spin = 4.168264 #Salsa
 #spin =4.210481 #Samba
 #spin =4.134934 #Tango
-
+spin = 0
 spins=[4.260732,4.168264,4.210481,4.134934]
 
-spins = [float('{:.2f}'.format(spin)) for spin in spins]
+spins = [float('{:.3f}'.format(spin)) for spin in spins]
 
-print spins
-print [spin*reset*10000 for spin in spins]
-
-min_ns = [int(spin*reset*10000) for spin in spins]
-  
+n=2000
+#spin=4.261
+reset = 5.152
+resetwidth = 0.085
+dt = 0.001
+#func()
+'''
 for spin in spins:
     func(spin,min_ns,axarr)
+'''
+f.suptitle('n:{0:04d} dt:{1:0.4f}'.format(n,dt),fontsize = 20)
+count = range(len(spins))
+for spin,row,c in zip(spins,axarr,count):
+    x,y = func()
+    print "max",max(x)
+    row[0].scatter(x,y,s=100,c='r')
+    row[0].set_title('spin:{:.4f}'.format(spin))
+    row[0].set_xlim((-max(x)*0.01,max(x)*1.1))   
+    freqs = np.fft.fftfreq(len(y),d=spin)[1:len(y)/2]
+    row[1].scatter(freqs,np.abs(np.fft.fft(y)[1:len(y)/2]))
+    row[1].set_title('FFT (excluding DC component)')
+    row[1].set_xlim((-max(freqs)*0.01,max(freqs)*1.1))
+    
+    if c == count[-1]:
+        row[0].set_xlabel('time (s)')
+        row[1].set_xlabel('frequency (1/s)')
+    
