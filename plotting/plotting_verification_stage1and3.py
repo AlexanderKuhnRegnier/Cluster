f98693b3e970c99bb8531b38926c3a92cc63cae3
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2016-07-14 15:34:51 +0100

    Finished implementing an interface to process data on the server, and then, using pickled python objects as well as the EXT.GSE file, plot this data on windows (but still with python) using matplotlib, since this is not available on the Cluster server. Multiple switches exist within the two scripts in order to modify the raw data produced as well as the way that it is plotted, ie. sine wave, gradient line, zoomed or not, etc...

--- plotting/plotting_verification_stage1and3.py
+++ plotting/plotting_verification_stage1and3.py
@@ -1,8 +1,8 @@
 import numpy as np
 import math
 import os
-import cPickle as pickle
-
+import pickle
+import datetime
 '''
 Needs to create .E0 raw data files:
 raw x,y,z values in engineering units, plus a range
@@ -50,23 +50,82 @@ def scale(r):
 #EXTfilename = 'Y:/testdata/test.EXT.GSE'
 
 #generate raw x,y,z data in engineering units
+
+'''
+#reset counter
+f = open('/home/ahk114/testdata/filenumber','w')
+f.write(str(0))
+f.close()  
+'''
+
+fn = 0
+f = open('/home/ahk114/testdata/filenumber','r')
+fn = int(f.read())
+f.close()
+f = open('/home/ahk114/testdata/filenumber','w')
+f.write(str(int(fn+1)))
+f.close()
+
+print "Filenumber:",fn
+fn = str(fn)
+'''
+filenamelist = ('C1_160122_B.EXT.GSE'+str(int(fn)-1),
+                'dt.pickle'+str(int(fn)-1),
+                'ExtProcDecoded_123',
+                'ExtProcRaw_123',
+                'magnitude_raw.pickle'+str(int(fn)-1),
+                'magnitude_scaled.pickle'+str(int(fn)-1),
+                'r_data.pickle'+str(int(fn)-1),
+                't_data.pickle'+str(int(fn)-1),
+                'test.E0',
+                'vector_scaled.pickle'+str(int(fn)-1),
+                'vectors_raw.pickle'+str(int(fn)-1))
+'''
+for f in os.listdir('/home/ahk114/testdata/'):
+    if '.EXT.GSE' in f or '.pickle' in f:
+        print "Removing:",f
+        os.remove(os.path.join('/home/ahk114/testdata/',f))
+
+print "Previous files removed!"
+
+
 mini = 10
 maxi = 2000
-t = np.linspace(0,10,num=50)
+
+dt = 0.4
+t = np.linspace(3.6,4,num=30)
+
 tshift1 = t+np.pi/3.
 tshift2 = t+2*np.pi/3.
 
-#x = (np.sin(t)+1)
-#y = (np.sin(tshift1)+1)
-#z = (np.sin(tshift2)+1)
+sin=False
 
-x = 1*np.ones((t.shape[0],1))
-y = 1*np.ones((t.shape[0],1))
-z = 1*np.ones((t.shape[0],1))
+if sin==True:
+    x = (np.sin(t)+1).reshape(t.shape[0],1)*5000
+    y = (np.sin(tshift1)+1).reshape(t.shape[0],1)*5000
+    z = (np.sin(tshift2)+1).reshape(t.shape[0],1)*5000
+else:
+    num = t.shape[0]
+    gradient = 1
+    offset =1000
+    scalex=20
+    scaley=10
+    scalez=10
+    scales = [scalex,scaley,scalez]
+    if abs(np.max(scales)*(gradient)*num+offset) > 65000:
+        raise Exception("Gradient,scale and offset are too large")
+    x = scalex*(np.linspace(0,gradient*num,num=num)+offset).reshape(-1,1)
+    y = scaley*(np.linspace(0,gradient*num,num=num)+offset).reshape(-1,1)
+    z = scalez*(np.linspace(0,gradient*num,num=num)+offset).reshape(-1,1)
 
+print "max, min values:"
+print np.max(x),np.min(x)
+print np.max(y),np.min(y)
+print np.max(z),np.min(z)
 
 #10%period
-p10 = t.shape[0]/2
+div = 1  #the higher div, the more range changes there are!!
+p10 = t.shape[0]/int(div)
 rlist = [2,3,4,5,6,7]
 c = -1
 r = []
@@ -81,19 +140,27 @@ for i in range(t.shape[0]):
 testdata = '/home/ahk114/testdata/'
 
 picklefilename = testdata+'t_data.pickle'
-pickle.dump(t,open(picklefilename,'wb'))
+f = open(picklefilename+fn,'w')
+pickle.dump(t,f)
+f.close()
 
 picklefilename = testdata+'r_data.pickle'
-pickle.dump(r,open(picklefilename,'wb'))
+f = open(picklefilename+fn,'w')
+pickle.dump(r,f)
+f.close()
 
 vectors_raw = np.hstack((x,y,z))
 magnitude_raw = np.array([],dtype=np.float64).reshape(1,0)
 for i in range(vectors_raw.shape[0]):
     magnitude_raw = np.append(magnitude_raw,np.linalg.norm(vectors_raw[i,:]))
 picklefilename = testdata+'vectors_raw.pickle'
-pickle.dump(vectors_raw,open(picklefilename,'wb'))
+f = open(picklefilename+fn,'w')
+pickle.dump(vectors_raw,f)
+f.close()
 picklefilename = testdata+'magnitude_raw.pickle'
-pickle.dump(magnitude_raw,open(picklefilename,'wb'))
+f = open(picklefilename+fn,'w')
+pickle.dump(magnitude_raw,f)
+f.close()
 
 Enfilename = '/home/ahk114/testdata/test.E0'
 def writeEntest():
@@ -123,7 +190,7 @@ for rvalue,v in zip(r,vector):
     for i in range(3):
         value = v[i]
         if value>32767:
-            print "negative"
+            #print "negative"
             value = (v[i]-65536.)/scale(rvalue)
         else:
             value = (v[i])/scale(rvalue)
@@ -133,9 +200,13 @@ magnitude = np.array([],dtype=np.float64).reshape(1,0)
 for i in range(vector.shape[0]):
     magnitude = np.append(magnitude,np.linalg.norm(vector[i,:]))
 picklefilename = testdata+'vector_scaled.pickle'
-pickle.dump(vector,open(picklefilename,'wb'))
+f = open(picklefilename+fn,'w')
+pickle.dump(vector,f)
+f.close()
 picklefilename = testdata+'magnitude_scaled.pickle'
-pickle.dump(magnitude,open(picklefilename,'wb'))
+f = open(picklefilename+fn,'w')
+pickle.dump(magnitude,f)
+f.close()
 
 '''
 All event data pertains to 160123 BS data, on 160122!
@@ -148,16 +219,26 @@ PROC = '/home/ahk114/testdata/'
 
 sattfile=RAW+'2016/01/'+'C'+'1'+'_'+'160122'+'_'+'B'+'.SATT' #Spacecraft Attitude and Spin Rates
 stoffile=RAW+'2016/01/'+'C'+'1'+'_'+'160122'+'_'+'B'+'.STOF'
-procfile=PROC+'C'+'1'+'_'+'160122'+'_'+'B'+'.EXT.GSE' #Where the data goes - into the reference folder!
+procfile=PROC+'C'+'1'+'_'+'160122'+'_'+'B'+'.EXT.GSE'+fn #Where the data goes - into the reference folder!
 
 
 tmp = '/home/ahk114/testdata/ExtProcRaw_123'
 tmp2 = '/home/ahk114/testdata/ExtProcDecoded_123'
 datahandle = open(tmp,'wb')
 
+
+picklefilename = testdata+'dt.pickle'
+f = open(picklefilename+fn,'w')
+pickle.dump(dt,f)
+f.close()
+
+
+print "Start time:",datetime.datetime.fromtimestamp(EventTime_Unix).strftime('%Y-%m-%dT%H:%M:%S')
+print "End time:",datetime.datetime.fromtimestamp(EventTime_Unix+dt*len(t)).strftime('%Y-%m-%dT%H:%M:%S')
+
 for i in range(len(t)):
-    #timestring = datetime.datetime.fromtimestamp(EventTime_Unix+1).strftime('%Y-%m-%d\T%H:%M:%S)
-    time = EventTime_Unix+i*0.01
+    #timestring = datetime.datetime.fromtimestamp(EventTime_Unix+1).strftime('%Y-%m-%dT%H:%M:%S')
+    time = EventTime_Unix+i*dt
     hexbx = float2hex(vector[i,0])
     hexby = float2hex(vector[i,1])
     hexbz = float2hex(vector[i,2])
@@ -172,7 +253,7 @@ for i in range(len(t)):
     #else:
         #value = [204, 76, 72, 70]#200.3
     '''
-    value = float2hex(100.15)
+    value = float2hex(1000.15)
     hexbx = value
     hexby = value
     hexbz = value
@@ -214,6 +295,7 @@ os.system(cmd)
 print ""
 print "Finished Executing"
 print ""
+print "dt:",dt," sine:",sin," length:",t.shape[0]
 #output = subprocess.check_output(cmd,shell=True)
 #output = subprocess.Popen(cmd,stdout=subprocess.PIPE).communicate()[0]
 #output = subprocess.Popen(cmd)
--- plotting/plotting_verification_stage1and3_matplotlib.py
+++ plotting/plotting_verification_stage1and3_matplotlib.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-#import datetime
+import datetime
 import math
 import os
-import cPickle as pickle
+import pickle
 
 
 '''
@@ -47,23 +47,65 @@ def scale(r):
     scale_list = [1024.,256.,64.,16.,4.,1.,.25]
     return scale_list[r-1]
     
+'''
+#reset counter
+f = open('Y:/Cluster/plotting/filenumber','w')
+f.write(str(0))
+f.close()  
+'''
+'''
+fn = 0
+f = open('Y:/Cluster/plotting/filenumber','r')
+fn = int(f.read())
+f.close()
+f = open('Y:/Cluster/plotting/filenumber','w')
+f.write(str(int(fn+1)))
+f.close()
+'''
 testdata = 'Y:/testdata/'  
+for f in os.listdir(testdata):
+    if '.pickle' in f:
+        fn = f.split('pickle')[-1]
+        print "Found pickle file:",f," filenumber:",fn
+        break
+#print "Filenumber:",fn
+#fn = str(fn)
+    
+
 #testdata = '/home/ahk114/testdata/'
 
-plt.close('all')
+#plt.close('all')
 #os.system('python /home/ahk114/Cluster/plotting/plotting_verification_stage1and3.py')
 
+picklefilename = testdata+'t_data.pickle'+fn
+print "Trying to open:",picklefilename
+f = open(picklefilename,'r')
+t = pickle.load(f)
+f.close()
+print "Loaded t data, length:",len(t)
 
-picklefilename = testdata+'t_data.pickle'
-t = pickle.load(open(picklefilename,'rb'))
-picklefilename = testdata+'r_data.pickle'
-r = pickle.load(open(picklefilename,'rb'))
-picklefilename = testdata+'vectors_raw.pickle'
-vectors = pickle.load(open(picklefilename,'rb'))
-picklefilename = testdata+'magnitude_raw.pickle'
-magnitude_raw =  pickle.load(open(picklefilename,'rb'))
+picklefilename = testdata+'r_data.pickle'+fn
+print "Trying to open:",picklefilename
+f = open(picklefilename,'r')
+r = pickle.load(f)
+f.close()
+print "Loaded r data, len:",len(r)
 
+picklefilename = testdata+'vectors_raw.pickle'+fn
+print "Trying to open:",picklefilename
+f = open(picklefilename,'r')
+vectors = pickle.load(f)
+f.close()
+print "Loaded raw vectors, shape:",vectors.shape
 
+picklefilename = testdata+'magnitude_raw.pickle'+fn
+print "Trying to open:",picklefilename
+f = open(picklefilename,'r')
+magnitude_raw =  pickle.load(f)
+f.close()
+print "Loaded raw mags, shape:",magnitude_raw.shape
+
+'''
 f,axes = plt.subplots(4,1,figsize=(20,15))
 axes[0].scatter(t,vectors[:,0],color='r')
 axes[0].set_title(list(set(vectors[:,0]))[:8])
@@ -74,18 +116,27 @@ axes[2].set_title(list(set(vectors[:,2]))[:8])
 axes[3].scatter(t,magnitude_raw,color='y')
 axes[3].set_title(list(set(magnitude_raw))[:8])
 f.canvas.set_window_title('Raw')
-
+'''
 ###################
 '''
 Stage3 recreation here
 '''
 ###################
 
-picklefilename = testdata+'vector_scaled.pickle'
-vector=pickle.load(open(picklefilename,'rb'))
-picklefilename = testdata+'magnitude_scaled.pickle'
-magnitude= pickle.load(open(picklefilename,'rb'))
+picklefilename = testdata+'vector_scaled.pickle'+fn
+print "Trying to open:",picklefilename
+f = open(picklefilename,'r')
+vector=pickle.load(f)
+f.close()
+print "Loaded scaled vector, shape:",vector.shape
 
+picklefilename = testdata+'magnitude_scaled.pickle'+fn
+print "Trying to open:",picklefilename
+f = open(picklefilename,'r')
+magnitude= pickle.load(f)
+f.close()
+print "Loaded scaled mags, shape:",magnitude.shape
+'''
 f,axes = plt.subplots(4,1,figsize=(20,15))
 axes[0].scatter(t,vector[:,0],color='r')
 axes[0].set_title(list(set(vector[:,0]))[:8])
@@ -96,29 +147,33 @@ axes[2].set_title(list(set(vector[:,2]))[:8])
 axes[3].scatter(t,magnitude,color='y')
 axes[3].set_title(list(set(magnitude))[:8])
 f.canvas.set_window_title('Converted')
-
+'''
 #PROC = '/home/ahk114/testdata/'
 PROC = 'Y:/testdata/'
-procfile=PROC+'C'+'1'+'_'+'160122'+'_'+'B'+'.EXT.GSE' #Where the data goes - into the reference folder!
+procfile=PROC+'C'+'1'+'_'+'160122'+'_'+'B'+'.EXT.GSE'+fn #Where the data goes - into the reference folder!
 
 extvector = np.array([],dtype=np.float64).reshape(-1,3)
 extmags = np.array([],dtype=np.float64)
 
-with open(procfile,'rb') as f:
+with open(procfile,'r') as f:
 	count = 0
 	for line in f:
 		#print "line", line
 		count += 1
 		try:
 			array = [i for i in line.split(" ") if i != '']
-			if 10<count<20:
-				print "EXT GSE array",array
+			#if 10<count<20:
+				#print "EXT GSE array",array
 			extvector=np.vstack((extvector,np.array([float(array[1]),float(array[2]),float(array[3])])))
 			extmags = np.append(extmags,np.linalg.norm(extvector[-1:][0]))
 		except ValueError:
 			print "Something wrong with line"
+   
+print "Read procfile:",procfile
+print "extvector, shape:",extvector.shape
+print "extmags,shape:",extmags.shape
 
-
+'''
 f,axes = plt.subplots(4,1,figsize=(20,15))
 axes[0].scatter(t,extvector[:,0],color='r')
 axes[0].set_title(list(set(extvector[:,0]))[:3])
@@ -129,40 +184,228 @@ axes[1].set_title(list(set(extvector[:,2]))[:3])
 axes[3].scatter(t,extmags,color='y')
 axes[3].set_title(str(list(set(extmags))[:3])+' '+str(len(set(extmags))))
 f.canvas.set_window_title('After DP pipeline, caution: axes matched to raw data axes')			
-#f.savefig(testdata+'0.02improvedtimestep100.15input.png')
+f.savefig(testdata+'10.0timestep1000.15input.png')
+'''
+plt.close('all')
 
+picklefilename = testdata+'dt.pickle'
+print "Trying to open:",picklefilename
+f = open(picklefilename+fn,'r')
+dt = pickle.load(f)
+f.close()
+print "Loaded dt, value:",dt
 
-f,axes = plt.subplots(4,3,figsize=(20,15))
-axes[0,0].scatter(t,vectors[:,0],color='r')
-axes[0,0].set_title(sorted(list(set(vectors[:,0]))[:8]))
-axes[1,0].scatter(t,vectors[:,1],color='b')
-axes[1,0].set_title(sorted(list(set(vectors[:,1]))[:8]))
-axes[2,0].scatter(t,vectors[:,2],color='g')
-axes[2,0].set_title(sorted(list(set(vectors[:,2]))[:8]))
-axes[3,0].scatter(t,magnitude_raw,color='y')
-axes[3,0].set_title(sorted(list(set(magnitude_raw))[:8]))
-#f.canvas.set_window_title('Raw')
+timesd = [datetime.datetime(1,1,1,0,0,0)+i*datetime.timedelta(microseconds=dt*1e6) for i in range(t.shape[0])]
+#print times
+times = [ti.time() for ti in timesd]
+#print times
 
-axes[0,1].scatter(t,vector[:,0],color='r')
-axes[0,1].set_title(sorted(list(set(vector[:,0]))[:8]))
-axes[1,1].scatter(t,vector[:,1],color='b')
-axes[1,1].set_title(sorted(list(set(vector[:,1]))[:8]))
-axes[2,1].scatter(t,vector[:,2],color='g')
-axes[2,1].set_title(sorted(list(set(vector[:,2]))[:8]))
-axes[3,1].scatter(t,magnitude,color='y')
-axes[3,1].set_title(sorted(list(set(magnitude))[:8]))
-#f.canvas.set_window_title('Converted')
+scattersize = 50
 
-axes[1,2].scatter(t,extvector[:,0],color='r')
-axes[1,2].set_title(sorted(list(set(extvector[:,0]))[:3],key=abs))
-axes[2,2].scatter(t,extvector[:,1],color='b')
-axes[2,2].set_title(sorted(list(set(extvector[:,1]))[:3],key=abs))
-axes[0,2].scatter(t,extvector[:,2],color='g')
-axes[0,2].set_title(sorted(list(set(extvector[:,2]))[:3],key=abs))
-axes[3,2].scatter(t,extmags,color='y')
-axes[3,2].set_title(str(sorted(list(set(extmags))[:3]))+' '+str(len(set(extmags))))
-f.canvas.set_window_title('Raw   ----     Converted   -----   After DP pipeline, caution: axes matched to raw data axes')			
+mod = 1 #if zoomed in or not!!
+
+save = 0
+filename = 'slope test2'
+
+min_scale = 0.999
+max_scale = 1.001
+
+if mod:
+    fig,axes = plt.subplots(4,3,figsize=(20,15))
+
+    start = 0
+    end = len(times)
+    #start=18
+    #end = 20
+ 
+    axes[1,0].scatter(times[start:end],extvector[:,0][start:end],color='r',s=scattersize)
+    axes[1,0].set_ylim((np.min(extvector[:,0][start:end])*min_scale,np.max(extvector[:,0][start:end])*max_scale))
+    #axes[1,2].set_title(sorted(list(set(extvector[:,0]))[:1],key=abs))
+    axes[2,0].scatter(times[start:end],-1*extvector[:,1][start:end],color='b',s=scattersize)
+    axes[2,0].set_ylim((np.min(-1*extvector[:,1][start:end])*min_scale,np.max(-1*extvector[:,1][start:end])*max_scale))
+    #axes[2,2].set_title(sorted(list(set(extvector[:,1]))[:1],key=abs))
+    axes[0,0].scatter(times[start:end],-1*extvector[:,2][start:end],color='g',s=scattersize)
+    axes[0,0].set_ylim((np.min(-1*extvector[:,2][start:end])*min_scale,np.max(-1*extvector[:,2][start:end])*max_scale))
+    #axes[0,2].set_title(sorted(list(set(extvector[:,2]))[:1],key=abs))
+    axes[3,0].scatter(times[start:end],extmags[start:end],color='y',s=scattersize)
+    axes[3,0].set_ylim((np.min(extmags[start:end])*min_scale,np.max(extmags[start:end])*max_scale))
+    #axes[3,2].set_title(str(sorted(list(set(extmags))[:1]))+' '+str(len(set(extmags))))
+    
+    start = 0
+    end = len(times)
+    start=0
+    end = 15
+    
+    axes[1,1].scatter(times[start:end],extvector[:,0][start:end],color='r',s=scattersize)
+    axes[1,1].set_ylim((np.min(extvector[:,0][start:end])*min_scale,np.max(extvector[:,0][start:end])*max_scale))
+    #axes[1,2].set_title(sorted(list(set(extvector[:,0]))[:1],key=abs))
+    axes[2,1].scatter(times[start:end],-1*extvector[:,1][start:end],color='b',s=scattersize)
+    axes[2,1].set_ylim((np.min(-1*extvector[:,1][start:end])*min_scale,np.max(-1*extvector[:,1][start:end])*max_scale))
+    #axes[2,2].set_title(sorted(list(set(extvector[:,1]))[:1],key=abs))
+    axes[0,1].scatter(times[start:end],-1*extvector[:,2][start:end],color='g',s=scattersize)
+    axes[0,1].set_ylim((np.min(-1*extvector[:,2][start:end])*min_scale,np.max(-1*extvector[:,2][start:end])*max_scale))
+    #axes[0,2].set_title(sorted(list(set(extvector[:,2]))[:1],key=abs))
+    axes[3,1].scatter(times[start:end],extmags[start:end],color='y',s=scattersize)
+    axes[3,1].set_ylim((np.min(extmags[start:end])*min_scale,np.max(extmags[start:end])*max_scale))
+    #axes[3,2].set_title(str(sorted(list(set(extmags))[:1]))+' '+str(len(set(extmags))))
+
+    start = 0
+    end = len(times)
+    start=15
+    #end = 5
+    
+    axes[1,2].scatter(times[start:end],extvector[:,0][start:end],color='r',s=scattersize)
+    axes[1,2].set_ylim((np.min(extvector[:,0][start:end])*min_scale,np.max(extvector[:,0][start:end])*max_scale))
+    #axes[1,2].set_title(sorted(list(set(extvector[:,0]))[:1],key=abs))
+    axes[2,2].scatter(times[start:end],-1*extvector[:,1][start:end],color='b',s=scattersize)
+    axes[2,2].set_ylim((np.min(-1*extvector[:,1][start:end])*min_scale,np.max(-1*extvector[:,1][start:end])*max_scale))
+    #axes[2,2].set_title(sorted(list(set(extvector[:,1]))[:1],key=abs))
+    axes[0,2].scatter(times[start:end],-1*extvector[:,2][start:end],color='g',s=scattersize)
+    axes[0,2].set_ylim((np.min(-1*extvector[:,2][start:end])*min_scale,np.max(-1*extvector[:,2][start:end])*max_scale))
+    #axes[0,2].set_title(sorted(list(set(extvector[:,2]))[:1],key=abs))
+    axes[3,2].scatter(times[start:end],extmags[start:end],color='y',s=scattersize)
+    axes[3,2].set_ylim((np.min(extmags[start:end])*min_scale,np.max(extmags[start:end])*max_scale))
+    #axes[3,2].set_title(str(sorted(list(set(extmags))[:1]))+' '+str(len(set(extmags))))
+
+if not mod:
+    fig,axes = plt.subplots(4,3,figsize=(20,15))
+    
+    axes[0,0].scatter(times,vectors[:,0],color='r',s=scattersize)
+    #axes[0,0].set_title(sorted(list(set(vectors[:,0]))[:1]))
+    axes[1,0].scatter(times,vectors[:,1],color='b',s=scattersize)
+    #axes[1,0].set_title(sorted(list(set(vectors[:,1]))[:1]))
+    axes[2,0].scatter(times,vectors[:,2],color='g',s=scattersize)
+    #axes[2,0].set_title(sorted(list(set(vectors[:,2]))[:1]))
+    axes[3,0].scatter(times,magnitude_raw,color='y',s=scattersize)
+    #axes[3,0].set_title(sorted(list(set(magnitude_raw))[:1]))
+    
+    
+    axes[0,1].scatter(times,vector[:,0],color='r',s=scattersize)
+    #axes[0,1].set_title(sorted(list(set(vector[:,0]))[:1]))
+    axes[1,1].scatter(times,vector[:,1],color='b',s=scattersize)
+    #axes[1,1].set_title(sorted(list(set(vector[:,1]))[:1]))
+    axes[2,1].scatter(times,vector[:,2],color='g',s=scattersize)
+    #axes[2,1].set_title(sorted(list(set(vector[:,2]))[:1]))
+    axes[3,1].scatter(times,magnitude,color='y',s=scattersize)
+    #axes[3,1].set_title(sorted(list(set(magnitude))[:1]))
+    
+    start = 0
+    end = len(times)
+    axes[1,2].scatter(times[start:end],extvector[:,0][start:end],color='r',s=scattersize)
+    axes[1,2].set_ylim((np.min(extvector[:,0][start:end])*min_scale,np.max(extvector[:,0][start:end])*max_scale))
+    #axes[1,2].set_title(sorted(list(set(extvector[:,0]))[:1],key=abs))
+    axes[2,2].scatter(times[start:end],-1*extvector[:,1][start:end],color='b',s=scattersize)
+    axes[2,2].set_ylim((np.min(-1*extvector[:,1][start:end])*min_scale,np.max(-1*extvector[:,1][start:end])*max_scale))
+    #axes[2,2].set_title(sorted(list(set(extvector[:,1]))[:1],key=abs))
+    axes[0,2].scatter(times[start:end],-1*extvector[:,2][start:end],color='g',s=scattersize)
+    axes[0,2].set_ylim((np.min(-1*extvector[:,2][start:end])*min_scale,np.max(-1*extvector[:,2][start:end])*max_scale))
+    #axes[0,2].set_title(sorted(list(set(extvector[:,2]))[:1],key=abs))
+    axes[3,2].scatter(times[start:end],extmags[start:end],color='y',s=scattersize)
+    axes[3,2].set_ylim((np.min(extmags[start:end])*min_scale,np.max(extmags[start:end])*max_scale))
+    #axes[3,2].set_title(str(sorted(list(set(extmags))[:1]))+' '+str(len(set(extmags))))
+ 
+
+for i in range(4):
+    if not mod:
+        axes[i,0].set_ylabel('engineering units')
+    else:
+        axes[i,0].set_ylabel('m-field (nT)')
+    axes[i,1].set_ylabel('m-field (nT)')
+    axes[i,2].set_ylabel('m-field (nT)')
+
+axes[0,2].set_ylabel('m-field (nT) x-1')
+axes[2,2].set_ylabel('m-field (nT) x-1')
+
+if mod:
+    axes[0,1].set_ylabel('m-field (nT) x-1')
+    axes[2,1].set_ylabel('m-field (nT) x-1')
+    axes[0,0].set_ylabel('m-field (nT) x-1')
+    axes[2,0].set_ylabel('m-field (nT) x-1')    
+if not mod:
+    arr = vectors[:,0]
+    axes[0,0].set_title('Engineering Units\nx'+'  range: '+format(np.ptp(arr),'.3e'))
+    arr = vector[:,0]
+    axes[0,1].set_title('Scaled Engineering Units -> nT\nx'+'  range: '+format(np.ptp(arr),'.3e'))
+    arr = -1*extvector[:,2][start:end]
+    axes[0,2].set_title('Magnetic Field in GSE (nT)\nz'+'  range: '+format(np.ptp(arr),'.3e'))
+    arr = vectors[:,1]
+    axes[1,0].set_title('y'+'  range: '+format(np.ptp(arr),'.3e'))
+    arr = vector[:,1]
+    axes[1,1].set_title('y'+'  range: '+format(np.ptp(arr),'.3e'))
+    arr = extvector[:,0][start:end]
+    axes[1,2].set_title('x'+'  range: '+format(np.ptp(arr),'.3e'))
+    arr = vectors[:,2]
+    axes[2,0].set_title('z'+'  range: '+format(np.ptp(arr),'.3e'))
+    arr = vector[:,2]
+    axes[2,1].set_title('z'+'  range: '+format(np.ptp(arr),'.3e'))
+    arr = -1*extvector[:,1][start:end]
+    axes[2,2].set_title('y'+'  range: '+format(np.ptp(arr),'.3e'))
+    arr = magnitude_raw
+    axes[3,0].set_title('mag'+'  range: '+format(np.ptp(arr),'.3e'))
+    arr = magnitude
+    axes[3,1].set_title('mag'+'  range: '+format(np.ptp(arr),'.3e'))
+    arr = extmags[start:end]
+    axes[3,2].set_title('mag'+'  range: '+format(np.ptp(arr),'.3e'))
+if mod:
+    arr = vectors[:,0]
+    #axes[0,0].set_title('Magnetic Field in GSE (nT)\nz'+'  range: '+format(np.ptp(arr),'.3e'))
+    axes[0,0].set_title('Magnetic Field in GSE (nT)\nz')
+    arr = vector[:,0]
+    #axes[0,1].set_title('Magnetic Field in GSE (nT)\nz'+'  range: '+format(np.ptp(arr),'.3e'))
+    axes[0,1].set_title('Magnetic Field in GSE (nT)\nz')
+    arr = -1*extvector[:,2][start:end]
+    #axes[0,2].set_title('Magnetic Field in GSE (nT)\nz'+'  range: '+format(np.ptp(arr),'.3e'))
+    axes[0,2].set_title('Magnetic Field in GSE (nT)\nz')
+    
+    axes[1,0].set_title('x')
+    axes[1,1].set_title('x')
+    axes[1,2].set_title('x')
+    axes[2,0].set_title('y')
+    axes[2,1].set_title('y')
+    axes[2,2].set_title('y')
+    axes[3,0].set_title('mag')
+    axes[3,1].set_title('mag')
+    axes[3,2].set_title('mag')
+    
+plt.gcf().autofmt_xdate()
+#f.canvas.set_window_title('')
 figManager = plt.get_current_fig_manager()
 figManager.window.showMaximized()
-
+suptitle_string = ('Passing two dots into the software does not prove to cause '
+            'any problems, with low or large time difference'
+            '\n')
+suptitle_opt= (' timestep:'+format(dt,'.2f')+' number of points:'+format(len(times),'05d')+
+            ' total time difference:'
+            +str(timesd[-1]-timesd[0]))
+fig.suptitle(suptitle_string+suptitle_opt,fontsize = 16)
 plt.show()
+
+print len(times)
+print len(vector[:,0])
+print "timestep:",dt
+print timesd[-1]-timesd[0]
+
+from shutil import copy
+if save:
+    try:
+        new_dir = 'Y:/testdata/'+filename.replace(' ','')
+        os.mkdir(new_dir)
+        copy('Y:/Cluster/plotting/plotting_verification_stage1and3_matplotlib.py',new_dir)
+        copy('Y:/Cluster/plotting/plotting_verification_stage1and3.py',new_dir)
+        print "Saved Python scripts for future reference!"
+        print "Saved in Dir:",new_dir
+    except OSError:
+        print "Warning: directory Already created, not saving scripts again!"
+
+
+if save:
+    if mod:
+        filename += ' zoom '
+    else:
+        filename += ' no zoom '
+    filename += suptitle_opt.replace(':',' ')
+    while filename+'.png' in os.listdir(testdata):
+        filename += ' v1'
+    filename += '.png'
+    fig.savefig(testdata+filename,dpi=280)#
+    print "Saved as:",testdata+filename
