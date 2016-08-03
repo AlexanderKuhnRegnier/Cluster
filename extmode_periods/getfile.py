4f5b92e659cb0ef15a4dd0314d8126fb7dd37c77
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2016-08-03 15:06:31 +0100

    Changed the getfile method in order to retrieve all the necessary caa files. This retrieves too much data, however, which will then have to be filtered out in the data analysis code. This is an unavoidable consequence of the fact that the caa data files contain a whole orbit's worth of data.

--- extmode_periods/getfile.py
+++ extmode_periods/getfile.py
@@ -1,5 +1,7 @@
 #from datetime import date,time,datetime,timedelta
 import os
+import pandas as pd
+from datetime import datetime,timedelta
 
 def getfile(sc,Year,month,day,directory,ext=False):
     Year = str(Year)
@@ -17,8 +19,6 @@ def getfile(sc,Year,month,day,directory,ext=False):
     else:
         raise Exception("Not valid caa or reference directory")
 
-
-
 def getextreffile(sc,Year,month,day,directory):
     '''
     Called by the getfile method - not to be used on its own!
@@ -35,77 +35,117 @@ def getreffile(sc,Year,month,day,directory):
     '''
     Called by the getfile method - not to be used on its own!
     '''
-    #file_dict = {}
-    '''
-    if int(Year)>=2000:
-        year = str(Year)[2:4]
-    else:
-        year=str(Year)
-    '''
     year=Year[2:4]
-    #month = str(month)
-    #day = str(day)
     sc = str(sc)
-    '''
-    #old code previously used in all three methods, now replaced by faster methods!
-    for file in os.listdir(directory):
-        if "C"+str(sc) in file and year+month+day in file and ".P.GSE" in file:
-            version = file[10]  
-            #print "version:",version
-            file_dict[version]=file
-    l = file_dict.keys()
-    if len(l):
-        #print "number of files:",len(l)
-        l.sort()
-        #print "final version:",l[-1]
-        #print ""
-        file = directory+file_dict[l[-1]]
-        return file
-    else:
-        return 0
-    '''
     for version in ['K','B','A']:
         filepath = directory+'C'+sc+'_'+year+month+day+'_'+version+'.P.GSE'
         if os.path.isfile(filepath):
             return filepath
     return 0
 
+def dt_to_strings(dt):
+    return str(dt.year),format(dt.month,'02d'),format(dt.day,'02d')
+
+def caa_version_filter(file_list):
+    assert len(file_list)>2,"File list to be filtered should contain multiple"\
+                            +" versions, and thus be longer than 2"
+    dummy = {}
+    dummy['file_basis'] = [f[0:49] for f in file_list]
+    dummy['file_versions'] = [int(f[49:51]) for f in file_list]
+    dummy['remainder'] = [f[51:] for f in file_list]
+    files = pd.DataFrame(dummy)
+    files.sort_values('file_versions',inplace=True,ascending=False)
+    files.drop_duplicates(subset=['file_basis'],inplace=True,keep='first')
+    files['file_versions']=files['file_versions'].apply(func=lambda x: format(x,'02d'))
+    results = files.apply(func=lambda x:''.join(x.values.tolist()),axis=1).values
+    assert len(results)<=2,("Filtered list should only contain one or two files"
+    "(not {}), one for the orbit end, and one for the start!").format(len(results))
+    return results.tolist()
+
 def getcaafile(sc,Year,month,day,directory):   
     '''
     Called by the getfile method - not to be used on its own!
     '''
-    file_dict = {}
-    '''
-    if int(Year)<2000:
-        Year = int(Year)
-        Year+=2000
-        Year=str(Year)
-    else:
-        Year=str(Year) 
-    '''
-    #month = str(month)
-    #day = str(day)
     sc = str(sc)
-    for file in os.listdir(directory):
-        if 'C'+sc+'_CP_FGM_SPIN__'+Year+month+day in file and '.cef.gz' in file:
-            version=file[-9:-7]           
-            file_dict[version] = file
-    l = file_dict.keys()
-    if len(l):
-        l.sort()
-        file = directory+file_dict[l[-1]]
-        return file
+    files = [s for s in os.listdir(directory) if 'SPIN' in s and 'C'+sc in s
+                                                and 'cef.gz' in s]
+    files_string = ''.join(files)
+    current_date = datetime(int(Year),int(month),int(day))
+    str_start = 'C'+sc+'_CP_FGM_SPIN__'+Year+month+day
+    str_end = '_'+Year+month+day+'_'
+    results = []
+    if current_date.day<4:
+        '''
+        need to check whether one of our files is in a previous folder!
+        '''        
+        dir = directory[:-8]
+        prev_month = current_date-timedelta(days=5)
+        prev_directory = dir+str(prev_month.year)+'/'\
+                        +format(prev_month.month,'02d')+'/'
+        found = [s for s in os.listdir(prev_directory) if 'SPIN' in s 
+                                    and 'C'+sc in s 
+                                    and 'cef.gz' in s
+                                    and pd.Timestamp(s[32:40])>=current_date]
+        if len(found)>1:
+            found = caa_version_filter(found)
+            assert len(found)==1,("1 file should be found this way," 
+                                " not {}!").format(len(found))
+        results.extend([prev_directory+s for s in found])  
+    '''
+    if orbit ends on current date then get two files:
+        one that start on the current date, and the one that ends on 
+        the current date
+    '''
+    if str_start in files_string:
+        if [s for s in files if str_end in s[31:]]:
+            found = [s for s in files if (str_start in s) or (str_end in s[31:])]
+            if len(results)>2:
+                found = caa_version_filter(found)
+            assert len(found)==2,"Two files should be found here"
+            results.extend([directory+s for s in found])
+            assert len(results)==2,"Two files should have been found by now"
+        else:
+            assert len(results)==1,"We should only be here if a file has been"\
+                                +" found in a previous month's folder!"
+            found = [s for s in files if (str_start in s)]
+            if len(found)>1:
+                found = caa_version_filter(found)
+            results.extend([directory+s for s in found])
+            assert len(results)==2,"Two files should have been found by now"
+
     else:
-        return 0
+        assert str_end not in files_string,"There is an orbit ending here,"\
+                                        +"but no orbit start!" 
+        '''
+        search for files that start on previous day or up to 2 days before    
+        '''
+        dates = [current_date-timedelta(days=1)*(i+1) for i in xrange(3)]
+        for date in dates:
+            Year,month,day = dt_to_strings(date)
+            str_start = 'C'+sc+'_CP_FGM_SPIN__'+Year+month+day
+            if str_start in files_string:
+                found =  [s for s in files if str_start in s]
+                if len(found)>1:
+                    found = caa_version_filter(found)
+                assert len(found)==1,"There should only be one such file!"
+                results.extend([directory+s for s in found])
+                break
+        assert len(results)==1,"One file should have been found by now"
+    return results
+                
             
 '''
 Testing
 '''
 '''
+#print "results"
 dir = 'Z:/caa/ic_archive/'
 #print getfile(1,2016,1,1,dir)
 #print getfile(1,2015,12,31,dir)
 #refdir = "Z:/data/reference/" 
+
 for i in range(1,20):
+    print "new"
+    print 2015,1,i
     print getfile(1,2015,1,i,dir)
 '''
