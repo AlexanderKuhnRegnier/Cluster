95c299db93b0d859d587ee287c97ca269693cb20
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2016-08-06 19:45:21 +0100

    Added code to deal with the possible duplication of information in the .EXT and .EXT.GSE files upon repeated execution of the processing stage 3. The log files containing the names of files which have been appended to (and may therefore contain duplicated information) are read in this code, and the unique filenames extracted. For each of these files, duplicates are removed, keeping only the last entry (based on the date field). No resorting is performed in this code. It is indicated how many files could not be opened. This information is also written to the log file 'not_cleaned.log' in the logs/date_range_stage3/ directory.

---
+++ cli/clean_appended.py
@@ -0,0 +1,81 @@
+def drop_duplicates(filepath):
+    '''
+    Reads the file at filepath and removes duplicates from the entries,
+    based only on the date information in the file (first 23 characters)
+    The last entries are kept, in case new processing methods change the 
+    values, but not the dates somehow.
+    If dates are changed by a change in the processing code, then this 
+    way of identifying duplicates no longer works and those duplicates 
+    will remain!
+    
+    No sorting is performed, it is assumed that the original file contains
+    sorted entries
+    '''
+    lines = []
+    with open(filepath,'rb') as f:
+        lines=f.readlines()
+    lines.reverse()
+    seen = {}
+    pos = 0
+    for line in lines:
+        if line[:24] not in seen:
+            seen[line[:24]]=True
+            lines[pos]=line
+            pos+=1
+    del lines[pos:]
+    lines.reverse()
+    with open(filepath,'wb') \
+    as f:
+        f.writelines(lines)
+
+def unique_fileset(filepath):
+    '''
+    Returns an unordered set of filenames
+    '''
+    lines=[]
+    try:
+        with open(filepath,'rb') as f:
+            lines=f.readlines()
+    except IOError:
+        print filepath,"does not seem to exist!"
+        return False
+    return set([line.split(' ')[-1].rstrip() for line in lines])
+    
+def main():
+    import math,datetime
+    not_processed = []
+    log_dir = '/home/ahk114/logs/date_range_stage3/'
+    log_appended_files = ('ext_appended.log','ext_gse_appended.log')
+    for log_file in log_appended_files:
+        time = datetime.datetime.now()
+        time_str = time.strftime('%Y-%m-%d %H:%M:%S ')
+        files = unique_fileset(log_dir+log_file)
+        if files:
+            increment_every = int(len(files)/10)
+            if increment_every == 0:
+                increment_every = 1
+            s = int(math.ceil(len(files)/float(increment_every)))
+            counter = 0
+            start = datetime.datetime.now()
+            print "Cleaning files from:",log_file
+            print "Time (s) Completion"
+            for count,filepath in enumerate(files):
+                if count%increment_every==0:
+                    print format((datetime.datetime.now()-start).seconds,'04d')+\
+                                                                            ' s  ',
+                    print "|"+"-"*counter+"|".rjust(s-counter)
+                    counter+=1
+                try:
+                    drop_duplicates(filepath)
+                except IOError:
+                    not_processed.append(time_str+filepath+'\n')
+            print "Number of files not processed (/total files):",str(len(not_processed))+\
+                    "/"+str(len(files))
+    error_file = log_dir+'not_cleaned.log'
+    with open(error_file,'ab') as f:
+        f.writelines(not_processed)
+    print "To remove the appended.log files, execute the following:"
+    print "rm {0} ; rm {1} ;".format(*[log_dir+log_file for log_file in log_appended_files])
+    
+if __name__ == "__main__":
+    main()
